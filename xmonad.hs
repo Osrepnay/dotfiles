1bdf4425c3c716dbc230401a03b4c06a78aec4d2
Llerraf Gnem <lleerraaff@gmail.com>
2021-05-04 22:08:51 -0500

    updates or smth idk

--- xmonad.hs
+++ xmonad.hs
@@ -1,27 +1,45 @@
+import qualified Data.Map as Map
 import XMonad
 import XMonad.Hooks.ManageDocks
 import XMonad.Layout.Fullscreen
+import XMonad.Layout.Gaps
 import XMonad.Layout.NoBorders
+import XMonad.Layout.Spacing
 import XMonad.Util.EZConfig
 
+main :: IO ()
 main = xmonad $ docks $ fullscreenSupport def
     { modMask = mod4Mask
     , terminal = "termite"
-    , borderWidth = 3
+    , borderWidth = 2
     , normalBorderColor  = "#282828"
-    , focusedBorderColor = "#b8bb26"
-    , startupHook     = spawn "picom -b"
+    , focusedBorderColor = "#d79921"
     , layoutHook      = myLayoutHook
     , handleEventHook = fullscreenEventHook
     , manageHook      = fullscreenManageHook
-    } `additionalKeysP`
-        [ ("<Print>", spawn "flameshot gui")
-        , ("M-s", spawn "rofi -show drun")
-        , ("<XF86AudioLowerVolume>", spawn "pamixer -d 7")
-        , ("<XF86AudioRaiseVolume>", spawn "pamixer -i 7")
-        ]
+    , keys = \x -> myKeybindings x
+        `Map.union`
+        Map.fromList (filter
+            (((flip notElem) (defaultKeybindingsToRemove x)) . fst)
+            (Map.toList ((keys def) x)))
+    }
 
-myLayoutHook = smartBorders $ avoidStruts $
+myLayoutHook =
+    gaps [(U, 24)] $
+    spacingRaw True (Border 8 8 8 8) True (Border 8 8 8 8) True $
+    smartBorders $
+    avoidStruts $
     noBorders Full ||| tall
     where
         tall = Tall 1 0.1 0.5
+
+myKeybindings x = mkKeymap x
+    [ ("<Print>", spawn "flameshot gui")
+    , ("M-s", spawn "rofi -show drun")
+    , ("M-t", spawn $ terminal x)
+    , ("<XF86AudioLowerVolume>", spawn "pamixer -d 7")
+    , ("<XF86AudioRaiseVolume>", spawn "pamixer -i 7")
+    ]
+
+defaultKeybindingsToRemove x =
+    [(modMask x .|. shiftMask, xK_Return)] -- new terminal, replace with mod+t
