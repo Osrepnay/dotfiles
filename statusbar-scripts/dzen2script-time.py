52ec14fc14ae568c220c298570f081115e4f4fba
Llerraf Gnem <lleerraaff@gmail.com>
2021-11-24 08:09:51 -0600

    bulk

--- statusbar-scripts/dzen2script-time.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-import time
-
-while True:
-    curr_time = time.strftime("%a %b %d %I:%M:%S%p", time.localtime()).replace("AM", "pm").replace("PM", "pm")
-    statusbar = f"^p(_LEFT)^p(_RIGHT)^p(-275){curr_time}"
-    print(statusbar, flush = True)
-    time.sleep(0.9)
